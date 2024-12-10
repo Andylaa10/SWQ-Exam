f47dfb3ee99aed6a4c9fa7f592d8ef27f9de6220
MarcusIversen <marcus.iversen@lego.com>
2024-12-10 10:46:24 +0100

    models for node postOffice done

---
+++ node/PostOffice/src/models/Delivery.ts
@@ -0,0 +1,6 @@
+
+export enum DeliveryType {
+  STANDARD,
+  EXPRESS,
+  ECONOMY
+}
---
+++ node/PostOffice/src/models/Package.ts
@@ -0,0 +1,73 @@
+import {DeliveryType} from "./Delivery";
+
+class Package {
+  private id: number;
+  private height: number;
+  private width: number;
+  private length: number;
+  private weight: number;
+  private deliveryType: DeliveryType;
+
+  constructor(id: number, height: number, width: number, length: number, weight: number, deliveryType: DeliveryType) {
+    this.id = id;
+    this.height = height;
+    this.width = width;
+    this.length = length;
+    this.weight = weight;
+    this.deliveryType = deliveryType;
+  }
+
+  // Getters
+  public getId(): number {
+    return this.id;
+  }
+
+  public getHeight(): number {
+    return this.height;
+  }
+
+  public getWidth(): number {
+    return this.width;
+  }
+
+  public getLength(): number {
+    return this.length;
+  }
+
+  public getWeight(): number {
+    return this.weight;
+  }
+
+  public getDeliveryType(): DeliveryType {
+    return this.deliveryType;
+  }
+
+  // Setters
+  public setId(id: number): void {
+    this.id = id;
+  }
+
+  public setHeight(height: number): void {
+    this.height = height;
+  }
+
+  public setWidth(width: number): void {
+    this.width = width;
+  }
+
+  public setLength(length: number): void {
+    this.length = length;
+  }
+
+  public setWeight(weight: number): void {
+    this.weight = weight;
+  }
+
+  public setDeliveryType(deliveryType: DeliveryType): void {
+    this.deliveryType = deliveryType;
+  }
+
+  public toString(): string {
+    return `Package{id=${this.id}, height=${this.height}, width=${this.width}, length=${this.length}, weight=${this.weight}, deliveryType=${this.deliveryType}}`;
+  }
+}
